c89070166b3963b3a931f9fc3473796fb8b65f54
Ferenandios <www.terunplay@gmail.com>
2024-03-26 19:04:54 +0300

    Added Search-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Text from "./components/Text/Text";
 import { GlobalStyle } from "./styles";
+import Search from "./components/Search/Search";
 
 const App: FC = (): JSX.Element => {
   return (
     <>
       <GlobalStyle />
       <Text />
+      <Search />
     </>
   );
 };
