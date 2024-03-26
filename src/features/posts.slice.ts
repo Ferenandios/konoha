cea590a3ebb9872d675b49cf89970abef244fbbb
Ferenandios <www.terunplay@gmail.com>
2024-03-26 20:15:29 +0300

    Created two reducers

--- src/features/posts.slice.ts
+++ src/features/posts.slice.ts
@@ -1,10 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { type IState } from "../types/types";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { type IPost, type IState } from "../types/types";
+import axios from "axios";
+
+export const getPosts = createAsyncThunk(
+  "posts/getPosts",
+  async (_, { dispatch }) => {
+    const url: string = "https://jsonplaceholder.typicode.com/posts";
+    const res = await axios.get<IPost[]>(url);
+    dispatch(setPosts(res.data));
+  }
+);
 
 const initialState: IState = {
   posts: [],
 };
 
-const postsSlice = createSlice({ name: "posts", initialState, reducers: {} });
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    setPosts: (state, action: PayloadAction<IPost[]>) => {
+      state.posts = action.payload;
+    },
+  },
+});
 
 export default postsSlice.reducer;
+export const { setPosts } = postsSlice.actions;
