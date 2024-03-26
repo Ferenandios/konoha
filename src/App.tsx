eb94b6c43db57a205490022736d1520c372c7335
Ferenandios <www.terunplay@gmail.com>
2024-03-26 20:18:27 +0300

    Added getPosts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Text from "./components/Text/Text";
 import { GlobalStyle } from "./styles";
 import Search from "./components/Search/Search";
+import { useAppDispatch } from "./hooks";
+import { getPosts } from "./features/posts.slice";
+import Posts from "./components/Posts/Posts";
 
 const App: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
   return (
     <>
       <GlobalStyle />
       <Text />
       <Search />
+      <Posts />
     </>
   );
 };
