5e13e4164094884cf57053c219c0a845b2b8ce3c
Ferenandios <www.terunplay@gmail.com>
2024-03-26 15:51:10 +0300

    Created file for Title-component styles

---
+++ src/components/Text/Title/Title.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+  font-size: 60px;
+  text-align: center;
+  line-height: 68px;
+  overflow-wrap: break-word;
+  width: 100%;
+  min-height: 68px;
+  margin-top: 64px;
+  margin-bottom: 24px;
+`;
+
+export default StyledTitle;
