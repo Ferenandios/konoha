c0701158130d7325652d452bebfbf379435753ee
Ferenandios <www.terunplay@gmail.com>
2024-03-27 18:16:04 +0300

    Added Button-component

--- src/components/Posts/Post/Content/Content.tsx
+++ src/components/Posts/Post/Content/Content.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { StyledInner } from "./Content.styles";
 import Title from "./Title/Title";
 import Subtitle from "./Subtitle/Subtitle";
+import Button from "./Button/Button";
 
 const Content: FC<{ postId: number }> = ({ postId }): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const Content: FC<{ postId: number }> = ({ postId }): JSX.Element => {
       <StyledInner>
         <Title postId={postId} />
         <Subtitle postId={postId} />
+        <Button />
       </StyledInner>
     </>
   );
