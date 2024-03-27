e9040c02ea3b07652aef58ffe15dc5d1ecc362b7
Ferenandios <www.terunplay@gmail.com>
2024-03-27 10:14:23 +0300

    Changed props for Rates and Rate-component

--- src/components/Posts/Post/Content/Title/Rates/Rates.tsx
+++ src/components/Posts/Post/Content/Title/Rates/Rates.tsx
@@ -1,18 +1,22 @@
 import { FC } from "react";
 import { StyledInner } from "./Rates.styles";
-import { IRates } from "../../../../../../types/types";
+import { type IRates, type userRateType } from "../../../../../../types/types";
 import Rate from "./Rate/Rate";
+import { useAppSelector } from "../../../../../../hooks";
 
 const getRandomNumber = () => Math.floor(Math.random() * 51);
 
-const Rates: FC<{ rates: IRates }> = ({ rates }): JSX.Element => {
+const Rates: FC<{ postId: number }> = ({ postId }): JSX.Element => {
+  const { rates, userRate } = useAppSelector(
+    (state) => state.posts.posts[postId]
+  );
   return (
     <>
       <StyledInner>
         {rates ? (
           <>
-            <Rate type="likes" count={rates.likes} />
-            <Rate type="dislikes" count={rates.dislikes} />
+            <Rate type="likes" postId={postId} />
+            <Rate type="dislikes" postId={postId} />
           </>
         ) : (
           <>
