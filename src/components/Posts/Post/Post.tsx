2fff891c56b7d511dd0a8c3b25e02238dee0291c
Ferenandios <www.terunplay@gmail.com>
2024-03-27 07:04:37 +0300

    Added Content-component

--- src/components/Posts/Post/Post.tsx
+++ src/components/Posts/Post/Post.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import { StyledInner } from "../Posts.styles";
 import Image from "./Image/Image";
 import { type IPost } from "../../../types/types";
+import Content from "./Content/Content";
 
 const Post: FC<{ post: IPost }> = ({ post }): JSX.Element => {
   return (
     <>
       <StyledInner>
         <Image image={post.image} />
+        <Content body={post.body} title={post.title} rates={post.rates} />
       </StyledInner>
     </>
   );
