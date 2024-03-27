d2899ba1da0e51bce7e8005697fc72df02ebab36
Ferenandios <www.terunplay@gmail.com>
2024-03-27 18:50:37 +0300

    Added new type for props

--- src/components/Posts/Post/Image/Image.styles.ts
+++ src/components/Posts/Post/Image/Image.styles.ts
@@ -4,12 +4,18 @@ export const ImageAnsoc = styled.div`
   width: 100%;
   height: 600px;
   background-color: #dddddd;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
 `;
 
-export const PostImage = styled.img.attrs<{ image: string }>((props) => ({
-  src: props.image ? props.image : "https://placehold.co/1140x600",
-  alt: "Post Image",
-}))`
+export const PostImage = styled.img.attrs<{ image: string | undefined }>(
+  (props) => ({
+    src: props.image ? props.image : "https://placehold.co/1140x600",
+    alt: "Post Image",
+  })
+)`
   width: 100%;
   height: 100%;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
 `;
