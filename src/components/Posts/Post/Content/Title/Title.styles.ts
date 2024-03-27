0d7797202a1b0ad38bddbf8a1d3eb1f11123337e
Ferenandios <www.terunplay@gmail.com>
2024-03-27 07:06:40 +0300

    Created styles for Title-component in Posts-component

---
+++ src/components/Posts/Post/Content/Title/Title.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  min-height: 32px;
+`;
+
+export const StyledTitle = styled.h3`
+  font-size: 28px;
+  line-height: 32px;
+  max-width: 959px;
+`;
