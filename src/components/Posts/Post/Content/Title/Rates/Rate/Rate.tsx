d5db95b7884fd2cf50e5743904919014d7fda0ef
Ferenandios <www.terunplay@gmail.com>
2024-03-27 10:15:35 +0300

    Added Click handler & Changed props

--- src/components/Posts/Post/Content/Title/Rates/Rate/Rate.tsx
+++ src/components/Posts/Post/Content/Title/Rates/Rate/Rate.tsx
@@ -1,15 +1,29 @@
 import { FC } from "react";
-import { RateButton, RateCount, RateImage, RateInner } from "./Rate.styles";
+import { RateButton, RateCount, RateInner } from "./Rate.styles";
+import RateIcon from "./RateIcon/RateIcon";
+import { type IRateProps } from "../../../../../../../types/types";
+import { useAppSelector } from "../../../../../../../hooks";
 
-const Rate: FC<{ type: "likes" | "dislikes"; count: number }> = ({
+const Rate: FC<{ type: "likes" | "dislikes"; postId: number }> = ({
+  postId,
   type,
-  count,
 }): JSX.Element => {
+  const { userRate } = useAppSelector((state) => state.posts.posts[postId]);
+  const count = useAppSelector(
+    (state) => state.posts.posts[postId].rates[type]
+  );
+
+  const handleMouseUp = () => {
+    alert(false);
+  };
+
+  /* КАКАЯ-ТО НЕВНЯТНАЯ ХЕРЬ КАПАЙСЯ В СВОИХ ПРОПСАХ ЧЕЛИК ГУД ЛАК */
+
   return (
     <>
       <RateInner>
-        <RateButton>
-          <RateImage type={type} />
+        <RateButton onMouseUp={handleMouseUp}>
+          <RateIcon type={type} userRate={userRate} />
         </RateButton>
         <RateCount>{count}</RateCount>
       </RateInner>
