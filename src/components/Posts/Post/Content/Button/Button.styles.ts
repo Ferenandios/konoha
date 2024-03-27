6cce85e9a23ececa2e55127e38bfcbf85a1b3ef1
Ferenandios <www.terunplay@gmail.com>
2024-03-27 18:16:28 +0300

    Created styles for Button-component

---
+++ src/components/Posts/Post/Content/Button/Button.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const ButtonAnsoc = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+  height: 45px;
+`;
+
+export const StyledButton = styled.button`
+  cursor: pointer;
+  color: #0a0a0a;
+  line-height: 18.75px;
+  min-width: 150px;
+  max-width: 300px;
+  height: 45px;
+  background: none;
+  border: 2px solid #0a0a0a;
+  border-radius: 60px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 0 10px;
+`;
