b4b8a260aa406d5340b254cc8c26394a0f43280e
Ferenandios <www.terunplay@gmail.com>
2024-03-27 17:46:18 +0300

    Moved properties from other files to here

--- src/components/Posts/Post/Content/Title/Rates/Rate/Rate.tsx
+++ src/components/Posts/Post/Content/Title/Rates/Rate/Rate.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { RateButton, RateCount, RateInner } from "./Rate.styles";
 import RateIcon from "./RateIcon/RateIcon";
-import { type IRateProps } from "../../../../../../../types/types";
 import { useAppSelector } from "../../../../../../../hooks";
 
 const Rate: FC<{ type: "likes" | "dislikes"; postId: number }> = ({
@@ -9,16 +8,12 @@ const Rate: FC<{ type: "likes" | "dislikes"; postId: number }> = ({
   type,
 }): JSX.Element => {
   const { userRate } = useAppSelector((state) => state.posts.posts[postId]);
-  const count = useAppSelector(
-    (state) => state.posts.posts[postId].rates[type]
-  );
-
+  const { rates } = useAppSelector((state) => state.posts.posts[postId]);
+  const getRandomNumber = () => Math.floor(Math.random() * 51);
+  const count = rates ? rates[type] : getRandomNumber();
   const handleMouseUp = () => {
     alert(false);
   };
-
-  /* КАКАЯ-ТО НЕВНЯТНАЯ ХЕРЬ КАПАЙСЯ В СВОИХ ПРОПСАХ ЧЕЛИК ГУД ЛАК */
-
   return (
     <>
       <RateInner>
