e2571c46afe6bc8c1468ef3bf7272f0879e8f9a8
Ferenandios <www.terunplay@gmail.com>
2024-03-27 07:04:15 +0300

    Created new property rates in IPost

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,23 @@
+export interface IRates {
+  likes: number;
+  dislikes: number;
+}
+
 export interface IPost {
   userId: number;
   id: number;
   title: string;
   body: string;
   image: string;
+  rates: IRates;
 }
 
 export interface IState {
   posts: IPost[];
+}
+
+export interface IContentProps {
+  title: string;
+  body: string;
+  rates: IRates;
 }
