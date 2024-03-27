b246ebeeb8b1bc8df56cdb3a74681695106384d7
Ferenandios <www.terunplay@gmail.com>
2024-03-27 19:49:11 +0300

    Added Resonsive Posts & Added new styles

--- src/components/Posts/Posts.styles.ts
+++ src/components/Posts/Posts.styles.ts
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const StyledInner = styled.div`
   display: flex;
+  justify-content: space-between;
   flex-wrap: wrap;
   width: 100%;
+  margin-top: 24px;
+  &:first-child {
+    margin-top: 0;
+  }
 `;
