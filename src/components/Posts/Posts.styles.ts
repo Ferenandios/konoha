371d3aa12fd064199f5db1ce4981cd9c461f4cab
Ferenandios <www.terunplay@gmail.com>
2024-03-28 13:16:43 +0300

    Created new components for posts

--- src/components/Posts/Posts.styles.ts
+++ src/components/Posts/Posts.styles.ts
@@ -2,12 +2,24 @@ import styled from "styled-components";
 
 export const StyledInner = styled.div`
   display: flex;
-  justify-content: space-between;
   flex-wrap: wrap;
   width: 100%;
+  gap: 24px;
   margin-top: 24px;
-  gap: 24px 0;
   &:first-child {
     margin-top: 0;
   }
+`;
+
+export const PostColumns = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 24px;
+`;
+
+export const PostColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
