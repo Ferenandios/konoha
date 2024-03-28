07c1bb2787c568ce032b5730bc75430a0e170092
Ferenandios <www.terunplay@gmail.com>
2024-03-28 14:42:34 +0300

    Moved getPosts function to Posts-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import Text from "./components/Text/Text";
 import { GlobalStyle } from "./styles";
 import Search from "./components/Search/Search";
-import { useAppDispatch } from "./hooks";
-import { getPosts } from "./features/posts.slice";
 import Posts from "./components/Posts/Posts";
 
 const App: FC = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(getPosts());
-  }, []);
   return (
     <>
       <GlobalStyle />
