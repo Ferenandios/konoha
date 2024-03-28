64c9a5066858d0b047d9a82cbc38a1ba4fd75aee
Ferenandios <www.terunplay@gmail.com>
2024-03-28 20:57:38 +0300

    Added styles for Responsive Text

--- src/components/BlogPage/Posts/Post/Content/Subtitle/Subtitle.styles.ts
+++ src/components/BlogPage/Posts/Post/Content/Subtitle/Subtitle.styles.ts
@@ -4,5 +4,8 @@ export const StyledText = styled.p`
   font-size: 24px;
   line-height: 32px;
   width: 100%;
+  max-height: 128px;
+  overflow: hidden;
+  text-overflow: ellipsis;
   margin-bottom: 32px;
 `;
