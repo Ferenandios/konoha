ecc405ca451abeeded204f21b52fb8642bb35053
Ferenandios <www.terunplay@gmail.com>
2024-03-28 17:24:11 +0300

    Created three new reducers

--- src/features/posts.slice.ts
+++ src/features/posts.slice.ts
@@ -1,3 +1,4 @@
+import { userRateType } from "./../types/types";
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { type IPost, type IState } from "../types/types";
 import axios from "axios";
@@ -22,8 +23,40 @@ const postsSlice = createSlice({
     setPosts: (state, action: PayloadAction<IPost[]>) => {
       state.posts = action.payload;
     },
+    setPostUserRate: (
+      state,
+      { payload }: PayloadAction<{ type: userRateType; postId: number }>
+    ) => {
+      state.posts[payload.postId].userRate = payload.type;
+    },
+    setPostRates: (
+      state,
+      {
+        payload,
+      }: PayloadAction<{
+        type: "add" | "remove";
+        rateType: "disliked" | "liked";
+        postId: number;
+      }>
+    ) => {
+      if (!("rates" in state.posts[payload.postId])) return;
+      if (!(payload.rateType in state.posts[payload.postId].rates)) return;
+      if (payload.type === "add") {
+        state.posts[payload.postId].rates[payload.rateType] += 1;
+      } else if (payload.type === "remove") {
+        state.posts[payload.postId].rates[payload.rateType] -= 1;
+      }
+    },
+    createPostRates: (state, action: PayloadAction<number>) => {
+      const getRandomNumber = () => Math.floor(Math.random() * 51);
+      state.posts[action.payload].rates = {
+        disliked: getRandomNumber(),
+        liked: getRandomNumber(),
+      };
+    },
   },
 });
 
 export default postsSlice.reducer;
-export const { setPosts } = postsSlice.actions;
+export const { setPosts, setPostUserRate, createPostRates, setPostRates } =
+  postsSlice.actions;
