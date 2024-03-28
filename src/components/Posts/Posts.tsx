9343ef2c082dadbba447055eaf3082cf35f1f09b
Ferenandios <www.terunplay@gmail.com>
2024-03-28 13:15:59 +0300

    Created particular function for columns

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,19 +1,40 @@
 import { FC } from "react";
 import { useAppSelector } from "../../hooks";
 import { GlobalContainer } from "../../styles";
-import { StyledInner } from "./Posts.styles";
+import { PostColumn, PostColumns, StyledInner } from "./Posts.styles";
 import Post from "./Post/Post";
 
 const Posts: FC = (): JSX.Element => {
   const { posts } = useAppSelector((state) => state.posts);
+  const columns = ((): number[][] => {
+    const oddNumbers: number[] = [];
+    const evenNumbers: number[] = [];
+
+    for (let i = 1; i < posts.length; i++) {
+      if (i % 2 !== 0) {
+        oddNumbers.push(i);
+      } else if (i !== 0) {
+        evenNumbers.push(i);
+      }
+    }
+
+    return [oddNumbers, evenNumbers];
+  })();
   return (
     <>
       <section>
         <GlobalContainer>
           <StyledInner>
-            {posts.map((post, index) => (
-              <Post key={post.id} postId={index} />
-            ))}
+            <Post postId={0} />
+            <PostColumns>
+              {columns.map((column, index) => (
+                <PostColumn key={index}>
+                  {column.map((postId) => (
+                    <Post key={postId} postId={postId} />
+                  ))}
+                </PostColumn>
+              ))}
+            </PostColumns>
           </StyledInner>
         </GlobalContainer>
       </section>
