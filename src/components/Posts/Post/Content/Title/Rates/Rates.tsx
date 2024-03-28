4c0e3577362bf7b2a2ac9e3a192b847f6cff1629
Ferenandios <www.terunplay@gmail.com>
2024-03-28 17:25:13 +0300

    Changed type prop to rateType prop

--- src/components/Posts/Post/Content/Title/Rates/Rates.tsx
+++ src/components/Posts/Post/Content/Title/Rates/Rates.tsx
@@ -1,24 +1,13 @@
 import { FC } from "react";
 import { StyledInner } from "./Rates.styles";
 import Rate from "./Rate/Rate";
-import { useAppSelector } from "../../../../../../hooks";
 
 const Rates: FC<{ postId: number }> = ({ postId }): JSX.Element => {
-  const { rates } = useAppSelector((state) => state.posts.posts[postId]);
   return (
     <>
       <StyledInner>
-        {rates ? (
-          <>
-            <Rate type="likes" postId={postId} />
-            <Rate type="dislikes" postId={postId} />
-          </>
-        ) : (
-          <>
-            <Rate type="likes" postId={postId} />
-            <Rate type="dislikes" postId={postId} />
-          </>
-        )}
+        <Rate rateType="liked" postId={postId} />
+        <Rate rateType="disliked" postId={postId} />
       </StyledInner>
     </>
   );
