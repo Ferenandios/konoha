cc9624a1f7e61aaf639f442243671ac9c2405e11
Ferenandios <www.terunplay@gmail.com>
2024-03-29 06:04:08 +0300

    Added Rates-component

--- src/components/PostPage/Top/Top.tsx
+++ src/components/PostPage/Top/Top.tsx
@@ -2,14 +2,18 @@ import { FC } from "react";
 import { GlobalContainer } from "../../../styles";
 import Button from "./Button/Button";
 import { TopInner } from "./Top.styles";
+import Rates from "./Rates/Rates";
+import { useAppSelector } from "../../../hooks";
 
 const Top: FC = (): JSX.Element => {
+  const { currentPost } = useAppSelector((state) => state.posts);
   return (
     <>
       <section>
         <GlobalContainer>
           <TopInner>
             <Button />
+            {currentPost !== null && <Rates postId={currentPost} />}
           </TopInner>
         </GlobalContainer>
       </section>
