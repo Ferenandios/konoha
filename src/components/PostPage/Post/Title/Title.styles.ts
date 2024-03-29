e1d61ba7e5498411a8457d2880e51afcf76fe9bb
Ferenandios <www.terunplay@gmail.com>
2024-03-29 06:33:06 +0300

    Created styles for Title-component in PostPage-component

---
+++ src/components/PostPage/Post/Title/Title.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const TitleAnsoc = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 48px;
+  margin-bottom: 48px;
+`;
+
+export const StyledTitle = styled.h1`
+  font-size: 40px;
+  line-height: 48px;
+  text-align: center;
+`;
