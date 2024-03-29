583429f16229dc1aec172ebab113a1cf6278868e
Ferenandios <www.terunplay@gmail.com>
2024-03-29 07:49:50 +0300

    Created styles for Text-component in PostPage-component

---
+++ src/components/PostPage/Post/Text/Text.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const TextAnsoc = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 26px;
+`;
+
+export const StyledText = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  width: 848px;
+`;
