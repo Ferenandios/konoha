bb630b4a644e1a8435a78b1d7d8c04b2fe7d429d
Ferenandios <www.terunplay@gmail.com>
2024-03-29 06:30:24 +0300

    Added currentPost checker & Added Post-component

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -1,10 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { setShowedPage } from "../../features/posts.slice";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import Top from "./Top/Top";
+import Post from "./Post/Post";
 
 const PostPage: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { currentPost } = useAppSelector((state) => state.posts);
+  useEffect(() => {
+    if (currentPost !== null) return;
+    dispatch(setShowedPage("blog"));
+  }, []);
   return (
     <>
       <Top />
+      <Post />
     </>
   );
 };
