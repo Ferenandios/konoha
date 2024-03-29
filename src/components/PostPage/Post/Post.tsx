79f404cc33451f80fb3e9a6549fad9da5b5f605a
Ferenandios <www.terunplay@gmail.com>
2024-03-29 07:49:10 +0300

    Added Text-component

--- src/components/PostPage/Post/Post.tsx
+++ src/components/PostPage/Post/Post.tsx
@@ -4,6 +4,7 @@ import { GlobalContainer } from "../../../styles";
 import { PostInner } from "./Post.styles";
 import Title from "./Title/Title";
 import Image from "./Image/Image";
+import Text from "./Text/Text";
 
 const Post: FC = (): JSX.Element => {
   const { currentPost } = useAppSelector((state) => state.posts);
@@ -15,6 +16,7 @@ const Post: FC = (): JSX.Element => {
             <PostInner>
               <Title postId={currentPost} />
               <Image postId={currentPost} />
+              <Text postId={currentPost} />
             </PostInner>
           </GlobalContainer>
         </section>
