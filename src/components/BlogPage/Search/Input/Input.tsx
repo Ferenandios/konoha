c98680a4606f197fb55504b603c037d3324cac89
Ferenandios <www.terunplay@gmail.com>
2024-03-30 06:46:55 +0300

    Created filter-by-title for posts

--- src/components/BlogPage/Search/Input/Input.tsx
+++ src/components/BlogPage/Search/Input/Input.tsx
@@ -1,16 +1,31 @@
-import { FC } from "react";
-import TextInput, { StyledDiv } from "./Input.styles";
+import { FC, useEffect, useState } from "react";
+import { useAppSelector } from "../../../../hooks";
 import Button from "./Button/Button";
+import TextInput, { StyledDiv } from "./Input.styles";
 
 const Input: FC = (): JSX.Element => {
-  const handleKeyUp = (event: React.KeyboardEvent) => {
-    if (event.key !== "Enter") return;
-    alert(true);
-  };
+  const { posts } = useAppSelector((state) => state.posts);
+  const [search, setSearch] = useState("");
+
+  const [filteredPosts, setFilterPosts] = useState(posts);
+
+  useEffect(() => {
+    setFilterPosts(posts.filter((post) => post.title === search));
+  }, [search, posts]);
   return (
     <>
       <StyledDiv>
-        <TextInput onKeyUp={handleKeyUp} />
+        <TextInput
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+        <div>
+          {!filteredPosts.length
+            ? posts.map((post) => <h3>{post.title}</h3>)
+            : filteredPosts.map((post) => <h3>{post.title}</h3>)}
+        </div>
         <Button />
       </StyledDiv>
     </>
