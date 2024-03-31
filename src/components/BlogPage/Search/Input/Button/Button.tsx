4cb59e9c9385c807e91a07122527124ba370bc7f
Ferenandios <www.terunplay@gmail.com>
2024-03-31 10:47:31 +0300

    Deleted handleClick function

--- src/components/BlogPage/Search/Input/Button/Button.tsx
+++ src/components/BlogPage/Search/Input/Button/Button.tsx
@@ -1,18 +1,11 @@
 import { FC } from "react";
 import StyledButton, { ShapeIcon, StyledAnsoc } from "./Button.styles";
-import { useAppDispatch, useAppSelector } from "../../../../../hooks";
-import { setFilteredPosts } from "../../../../../features/posts.slice";
 
 const Button: FC = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const { posts, search } = useAppSelector((state) => state.posts);
-  const handleClick = () => {
-    dispatch(setFilteredPosts(posts.filter((post) => post.title === search)));
-  };
   return (
     <>
       <StyledAnsoc>
-        <StyledButton onMouseUp={handleClick}>
+        <StyledButton>
           <ShapeIcon />
         </StyledButton>
       </StyledAnsoc>
