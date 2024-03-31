e6410c8dbb09909bb6bde98dfef54e5170d6fb29
Ferenandios <www.terunplay@gmail.com>
2024-03-31 09:57:38 +0300

    Changed props

--- src/components/BlogPage/Posts/Post/Post.tsx
+++ src/components/BlogPage/Posts/Post/Post.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 import Image from "./Image/Image";
 import Content from "./Content/Content";
 import { StyledInner } from "./Post.styles";
+import { useAppSelector } from "../../../../hooks";
 
 const Post: FC<{ postId: number }> = ({ postId }): JSX.Element => {
+  const { posts } = useAppSelector((state) => state.posts);
   return (
     <>
-      <StyledInner index={postId}>
+      <StyledInner type={posts[0].id === postId + 1 ? "bigPost" : "smallPost"}>
         <Image postId={postId} />
         <Content postId={postId} />
       </StyledInner>
