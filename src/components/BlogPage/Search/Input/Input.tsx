b7040911228bf33f765d02574355010f4d65d107
Ferenandios <www.terunplay@gmail.com>
2024-03-31 10:46:15 +0300

    Removed posts in useSelector

--- src/components/BlogPage/Search/Input/Input.tsx
+++ src/components/BlogPage/Search/Input/Input.tsx
@@ -1,28 +1,19 @@
 import { FC } from "react";
-import { setFilteredPosts, setSearch } from "../../../../features/posts.slice";
+import { setSearch } from "../../../../features/posts.slice";
 import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import Button from "./Button/Button";
 import TextInput, { StyledDiv } from "./Input.styles";
 
 const Input: FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const { search, posts } = useAppSelector((state) => state.posts);
+  const { search } = useAppSelector((state) => state.posts);
   return (
     <>
       <StyledDiv>
         <TextInput
           value={search}
           onChange={(event) => {
-            if (event.target.value === "") {
-              dispatch(setFilteredPosts([]));
-            }
             dispatch(setSearch(event.target.value));
-          }}
-          onKeyUp={(event: KeyboardEvent) => {
-            if (event.key !== "Enter") return;
-            dispatch(
-              setFilteredPosts(posts.filter((post) => post.title === search))
-            );
           }}
         />
         <Button />
