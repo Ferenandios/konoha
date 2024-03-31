55159d8c3af81f5420a297872c15f6e5ddb0867a
Ferenandios <www.terunplay@gmail.com>
2024-03-31 09:58:25 +0300

    Changed props

--- src/components/BlogPage/Posts/Post/Post.styles.ts
+++ src/components/BlogPage/Posts/Post/Post.styles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-export const StyledInner = styled.div<{ index: number }>`
+export const StyledInner = styled.div<{ type: "bigPost" | "smallPost" }>`
   display: flex;
   flex-direction: column;
   align-self: flex-start;
-  width: ${(props) => (!props.index ? "100%" : "558px")};
+  width: ${(props) => (props.type === "bigPost" ? "1140px" : "558px")};
   border: 1px solid #f4f4f4;
   border-radius: 12px;
   box-shadow: 0px 0px 1px 0px #0000000a;
